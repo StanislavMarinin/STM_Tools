4bab79f02aec4d4db842fe41ada55079df82ebfe
STM <95878397+StanislavMarinin@users.noreply.github.com>
2025-01-06 18:19:35 +0500

    Update gaia.sh

--- gaia.sh
+++ gaia.sh
@@ -48,7 +48,8 @@ EOF"
 execute_with_prompt "sudo systemctl daemon-reload"
 execute_with_prompt "source ~/.bashrc"
 execute_with_prompt "gaianet init --config 'https://raw.githubusercontent.com/GaiaNet-AI/node-configs/main/qwen2-0.5b-instruct/config.json'"
-execute_with_prompt "sudo systemctl restart gaianet.service"
+sudo systemctl restart gaianet.service
 execute_with_prompt "gaianet info > gaia.txt"
 execute_with_prompt "sudo systemctl status gaianet.service"
 execute_with_prompt "journalctl -u gaianet.service -f"
+
