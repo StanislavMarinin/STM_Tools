44944f54df589e4eee6bd297a8a7bfa569d3bd8d
STM <95878397+StanislavMarinin@users.noreply.github.com>
2025-01-08 13:07:26 +0500

    Update g2.sh

--- g2.sh
+++ g2.sh
@@ -16,71 +16,15 @@ execute_with_prompt() {
     fi
 }
 
-execute_with_prompt "sudo apt update -y sudo apt update"
+execute_with_prompt "sudo apt update -y && sudo apt update"
 execute_with_prompt "sudo apt install python3-pip -y"
 execute_with_prompt "sudo apt install nano -y"
 execute_with_prompt "sudo apt install screen -y"
 execute_with_prompt "pip install requests"
 execute_with_prompt "pip install faker"
-execute_with_prompt "WADR="https://0xf2b1c69192200774039623a2d7f3af0e360052b5.gaia.domains/""
-execute_with_prompt "sudo tee /root/random_chat_with_faker.py << EOF
-import requests
-import random
-import logging
-import time
-from faker import Faker
-from datetime import datetime
-
-node_url = "$WADR.us.gaianet.network/v1/chat/completions"
-
-faker = Faker()
-
-headers = {
-    "accept": "application/json",
-    "Content-Type": "application/json"
-}
-
-logging.basicConfig(filename='chat_log.txt', level=logging.INFO, format='%(asctime)s - %(message)s')
-
-def log_message(node, message):
-    logging.info(f"{node}: {message}")
-
-def send_message(node_url, message):
-    try:
-        response = requests.post(node_url, json=message, headers=headers)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        print(f"Failed to get response from API: {e}")
-        return None
-
-def extract_reply(response):
-    if response and 'choices' in response:
-        return response['choices'][0]['message']['content']
-    return ""
-
-while True:
-    random_question = faker.sentence(nb_words=10)
-    message = {
-        "messages": [
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": random_question}
-        ]
-    }
-
-    question_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    response = send_message(node_url, message)
-    reply = extract_reply(response)
-
-    reply_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    log_message("Node replied", f"Q ({question_time}): {random_question} A ({reply_time}): {reply}")
-
-    print(f"Q ({question_time}): {random_question}\nA ({reply_time}): {reply}")
-
-    delay = random.randint(60, 180)
-    time.sleep(delay)
-EOF"
-execute_with_prompt "screen -S faker_session"
-execute_with_prompt "python3 ~/random_chat_with_faker.py"
+execute_with_prompt "gaianet run"
+WADR=$(sed -n 's/ *\"address\": \"\([^\"]*\)\".*/\1/p' /root/gaianet/nodeid.json)
+execute_with_prompt "wget https://raw.githubusercontent.com/StanislavMarinin/STM_Tools/refs/heads/main/random_chat_with_faker.py"
+execute_with_prompt "sed -i 's/wall/$WADR/g' random_chat_with_faker.py"
+FCHAT="python3 ~/random_chat_with_faker.py"
+execute_with_prompt "tmux new-session -d -s fakerchat bash -c \"${FCHAT}\""
